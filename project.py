5c7aa10722284f8ca12848c37b3a026860114b69
Varvara <miss.v-ermolina@yandex.ru>
2020-12-23 22:00:24 +0300

    start1

---
+++ project.py
@@ -0,0 +1,38 @@
+import pygame
+import os
+import sys
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 800, 650
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Тамагочи')
+    bg = load_image("cub.jpg")
+    running = True
+    # clock = pygame.time.Clock()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                pass
+        pygame.display.flip()
+        screen.blit(bg, (0, 0))
+    pygame.quit()
